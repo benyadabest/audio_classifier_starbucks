ff5bc36096438656073c3460580ed87e8e044d31
Benjamin Shvartsman <code.blooded.ben@gmail.com>
2024-04-26 12:02:14 -0400

    live audio recording + starbucks menu

--- model.py
+++ model.py
@@ -1,42 +1,54 @@
 import requests
 import streamlit as st
+import io
 from st_audiorec import st_audiorec
+import string
 
 
 API_URL = "https://api-inference.huggingface.co/models/openai/whisper-large-v3"
-API_TOKEN = "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"
+API_TOKEN = "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"
 headers = {"Authorization": f"Bearer {API_TOKEN}"}
 
-def query(filename):
+def query_recor(audio_data):
+    response = requests.post(API_URL, headers=headers, data=audio_data)
+    return response.json()
+
+def query_file(filename):
     with open(filename, "rb") as f:
         data = f.read()
     response = requests.post(API_URL, headers=headers, data=data)
     return response.json()
 
+def normalize(text):
+    return text.lower().translate(str.maketrans('', '', string.punctuation))
+
 #testing
-output = query("choc_cookie_order.flac")
+output = query_file("choc_cookie_order.flac")
 print(output['text'])
 
 st.title("Audio Classifcation: Starbucks Order Automation")
-st.text("Speech -> Text -> Link to Item")
+st.text("Speak your order, and we'll transcribe it for you!")
 
-uploaded_file = st.file_uploader("Upload Audio File!")
+sample = st.button("Sample Order :musical_note::cookie:")
+
 wav_audio_data = st_audiorec()
 
 
 button = st.button("Order :coffee::croissant::white_check_mark::cool:")
 
 if button:
-    if uploaded_file is not None:
-        if wav_audio_data is not None:
-            st.audio(wav_audio_data, format='audio/wav')
-        with st.spinner("Loading..."):
-            with open("temp_audio.flac", "wb") as f:
-                f.write(uploaded_file.getvalue())
-
-                output = query("temp_audio.flac")
-                transcription = output['text'] if 'text' in output else "Error in transcription."
-
-                st.text_area("Transcription Result:", transcription, height=150)
+    if wav_audio_data is not None:
+        with st.spinner("Transcribing your order..."):
+            output = query_recor(wav_audio_data)
+            transcription = output['text'] if 'text' in output else "Error in transcription."
+            st.text_area("Transcription Result:", transcription, height=150)
     else:
-        st.error("Upload audio file first!!")
+        st.error("Please record your order first!")
+
+if sample:
+    with st.spinner("Transcribing your order..."):
+            st.audio("choc_cookie_order.flac")
+            output = query_file("choc_cookie_order.flac")
+            transcription = output['text'] if 'text' in output else "Error in transcription."
+            st.text_area("Transcription Result:", transcription, height=150)
+
